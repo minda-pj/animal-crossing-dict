b223b8931e6c7707821e9e9a59a550ee8373d8f6
Minho Yoo <ymh0951@naver.com>
2025-02-27 22:26:46 +0900

    Error 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import Villagers from "./components/Villagers";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      동물의 숲
-      <Villagers />
-    </div>
-  );
+  return <div className={styles.page}>동물의 숲</div>;
 }
