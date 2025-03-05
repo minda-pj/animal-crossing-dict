cc313108a2be9e5aff510c714ec6fab5dcdca1e6
Minho Yoo <ymh0951@naver.com>
2025-03-05 22:07:52 +0900

    Detail 퍼블리싱 완료

--- app/components/DetailModal.tsx
+++ app/components/DetailModal.tsx
@@ -1,14 +1,89 @@
 "use client";
 
 import {
+  DetailInfoCommentButton,
+  DetailInfoCommentInput,
+  DetailInfoCommentInputWrap,
+  DetailInfoCommentTitle,
+  DetailInfoCommentWrap,
+  DetailInfoImageWrap,
+  DetailInfoInner,
+  DetailInfoText,
+  DetailInfoTextBold,
+  DetailInfoTextLi,
+  DetailInfoTextUl,
+  DetailInfoWrap,
+  DetailModalCloseButton,
   DetailModalContent,
+  DetailModalTitle,
+  DetailModalTitleWrap,
   DetailModalWrap,
 } from "../styles/DetailModal.styles";
+import { X, Leaf } from "lucide-react";
 
 export const DetailModal = () => {
   return (
     <DetailModalWrap>
-      <DetailModalContent></DetailModalContent>
+      <DetailModalContent>
+        <DetailModalCloseButton>
+          <X className="close-icon" size={16} color="black" />
+        </DetailModalCloseButton>
+        <DetailModalTitleWrap>
+          <Leaf size={24} color="#7dce82" />
+          <DetailModalTitle>붕어</DetailModalTitle>
+          <Leaf size={24} color="#7dce82" />
+        </DetailModalTitleWrap>
+        <DetailInfoWrap>
+          <DetailInfoInner>
+            <DetailInfoImageWrap></DetailInfoImageWrap>
+            <DetailInfoTextUl>
+              <DetailInfoTextLi>
+                <DetailInfoText>
+                  <DetailInfoTextBold>가격:</DetailInfoTextBold>
+                  160 벨
+                </DetailInfoText>
+              </DetailInfoTextLi>
+              <DetailInfoTextLi>
+                <DetailInfoText>
+                  <DetailInfoTextBold>위치:</DetailInfoTextBold>강
+                </DetailInfoText>
+              </DetailInfoTextLi>
+              <DetailInfoTextLi>
+                <DetailInfoText>
+                  <DetailInfoTextBold>그림자 크기:</DetailInfoTextBold>
+                  작음
+                </DetailInfoText>
+              </DetailInfoTextLi>
+              <DetailInfoTextLi>
+                <DetailInfoText>
+                  <DetailInfoTextBold>출현 시간:</DetailInfoTextBold>
+                  항상
+                </DetailInfoText>
+              </DetailInfoTextLi>
+              <DetailInfoTextLi>
+                <DetailInfoText>
+                  <DetailInfoTextBold>북반구 출현 월:</DetailInfoTextBold>
+                  1월-12월
+                </DetailInfoText>
+              </DetailInfoTextLi>
+              <DetailInfoTextLi>
+                <DetailInfoText>
+                  <DetailInfoTextBold>남반구 출현 월:</DetailInfoTextBold>
+                  1월-12월
+                </DetailInfoText>
+              </DetailInfoTextLi>
+            </DetailInfoTextUl>
+          </DetailInfoInner>
+          <DetailInfoCommentWrap>
+            <DetailInfoCommentTitle>댓글</DetailInfoCommentTitle>
+            <DetailInfoCommentInputWrap>
+              <DetailInfoCommentInput placeholder="이름" />
+              <DetailInfoCommentInput placeholder="댓글 작성..." />
+              <DetailInfoCommentButton>댓글 작성</DetailInfoCommentButton>
+            </DetailInfoCommentInputWrap>
+          </DetailInfoCommentWrap>
+        </DetailInfoWrap>
+      </DetailModalContent>
     </DetailModalWrap>
   );
 };
--- app/styles/DetailModal.styles.ts
+++ app/styles/DetailModal.styles.ts
@@ -26,9 +26,130 @@ export const DetailModalWrap = styled.div`
 
 export const DetailModalContent = styled.article`
   width: 100%;
-  max-width: 896px;
+  max-width: 56rem;
   padding: 1.5rem;
-  border: 4px solid rgb(125, 206, 130);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  position: relative;
+  border: 0.25rem solid rgb(125, 206, 130);
   border-radius: 0.5rem;
   background-color: rgb(249, 245, 231);
+`;
+
+export const DetailModalCloseButton = styled.button`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+
+  .close-icon {
+    opacity: 0.7;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+`;
+
+export const DetailModalTitleWrap = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+export const DetailModalTitle = styled.h2`
+  font-weight: 600;
+  font-size: 1.875rem;
+  color: rgb(125, 206, 130);
+`;
+
+export const DetailInfoWrap = styled.div`
+  width: 100%;
+  padding: 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  box-sizing: border-box;
+`;
+
+export const DetailInfoInner = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.5rem;
+`;
+
+export const DetailInfoImageWrap = styled.div`
+  width: 192px;
+  height: 192px;
+  border: 0.25rem solid rgb(255, 245, 99);
+  border-radius: 62.4375rem;
+  box-shadow: 0 4px 20px #2445b426;
+  background-color: #eaeaea;
+`;
+
+export const DetailInfoTextUl = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const DetailInfoTextLi = styled.li``;
+
+export const DetailInfoText = styled.p`
+  font-size: 1.125rem;
+`;
+
+export const DetailInfoTextBold = styled.b`
+  margin-right: 5px;
+  font-weight: 700;
+`;
+
+export const DetailInfoCommentWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const DetailInfoCommentTitle = styled.h3`
+  font-weight: 400;
+  font-size: 1.5rem;
+  color: #7dce82;
+`;
+
+export const DetailInfoCommentInputWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+export const DetailInfoCommentInput = styled.input`
+  height: 2.5rem;
+  padding: 0.5rem 0.75rem;
+  border: 0.0625rem solid rgb(228, 228, 231);
+  border-radius: 0.375rem;
+  box-sizing: border-box;
+  font-size: 0.875rem;
+
+  &:focus {
+    outline: 0.0938rem solid #a5a5a5;
+  }
+`;
+
+export const DetailInfoCommentButton = styled.button`
+  width: fit-content;
+  height: 2.5rem;
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 0.375rem;
+  box-sizing: border-box;
+  background-color: rgb(125, 206, 130);
+  color: #fff;
+  cursor: pointer;
 `;
